145023b9bc8af36036aa5ec4efd78c14c4574282
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-18 17:37:38 -0500

    get accounts data implemented

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,32 @@
-exports.checkAccountPayload = (req, res, next) => {
+const Accounts = require('./accounts-model')
+
+
+const checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
+const checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
+const checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.getById(req.params.id)
+  .then(account => {
+    if(account) {
+      req.existingAccount = account
+      next()
+    } else {
+          res.status(404).json({ message: "account not found" })
+        }
+      })
+      .catch(error => next({ error }))
+}
+
+module.exports = {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,40 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
+const { checkAccountId } = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const data = await Accounts.getAll()
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  res.status(200).json(req.existingAccount)
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  res.send('not there yet')
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  res.send('not there yet')
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  res.send('not there yet')
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.send('not there yet')
 })
 
 module.exports = router;
